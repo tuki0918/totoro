13e90697365bbf1e68e61a2eed7aa8d29cbc703d
tuki0918 <tuki0918@gmail.com>
2016-05-03 16:34:33 +0900

    PHPUnitセットアップ

---
+++ tests/TwitterApiClientTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class TwitterApiClientTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function セットアップ()
+    {
+        static::assertEquals(true, true);
+    }
+}
